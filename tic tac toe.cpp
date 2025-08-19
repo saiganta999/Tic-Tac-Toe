94c0360ceeed4d20ec66d7b37ca2d44b3ea94417
saiganta999 <is23bm026@iitdh.ac.in>
2025-08-19 18:56:44 +0530

    Update tic tac toe.cpp

--- tic tac toe.cpp
+++ tic tac toe.cpp
@@ -15,11 +15,11 @@ void displayBoard(char board[3][3]) {
 bool checkWin(char board[3][3], char player) 
 {
     for (int i = 0; i < 3; i++) {
-        if (board[i][0] == player && board[i][1] == player && board[i][2] == player) return true; // Rows
-        if (board[0][i] == player && board[1][i] == player && board[2][i] == player) return true; // Columns
+        if (board[i][0] == player && board[i][1] == player && board[i][2] == player) return true; 
+        if (board[0][i] == player && board[1][i] == player && board[2][i] == player) return true; 
     }
-    if (board[0][0] == player && board[1][1] == player && board[2][2] == player) return true; // Diagonal 1
-    if (board[0][2] == player && board[1][1] == player && board[2][0] == player) return true; // Diagonal 2
+    if (board[0][0] == player && board[1][1] == player && board[2][2] == player) return true; 
+    if (board[0][2] == player && board[1][1] == player && board[2][0] == player) return true; 
     return false;
 }
 
@@ -80,3 +80,4 @@ int main() {
     return 0;
 
 }  
+
